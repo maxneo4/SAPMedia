f19f60b675bff19e70afb286a52bc4dca6c2e43a
maxneo4 <maxneo4@gmail.com>
2016-01-26 14:43:11 -0500

    Teniendo aun template y subtemplates separadas

--- src/services/sap_media_api.py
+++ src/services/sap_media_api.py
@@ -2,7 +2,8 @@ from flask import Flask, request
 import metrics_importation
 import sys
 from max_dev import maxrest
-from reports.publications.articles import report_per_year
+from reports.publications.articles import report_per_year as report_year_articles
+from reports.publications.videos import  report_per_year as report_year_videos
 
 app = Flask(__name__)
 
@@ -18,9 +19,17 @@ def home():
     return 'HOME'
 
 @app.route('/reports/publications/articles/per_year/<year>')
-def get_report_publications_per_year(year):
+def get_report_articles_per_year(year):
     try:
-        return report_per_year.generate_report(year)
+        return report_year_articles.generate_report(year)
+    except:
+        print "Unexpected error:", sys.exc_info()[0]
+        raise
+
+@app.route('/reports/publications/videos/per_year/<year>')
+def get_report_videos_per_year(year):
+    try:
+        return report_year_videos.generate_report(year)
     except:
         print "Unexpected error:", sys.exc_info()[0]
         raise
