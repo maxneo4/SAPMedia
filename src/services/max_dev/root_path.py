aa0dec76865c034824ea036e4261ac5fb6bcdec2
maxneo4 <maxneo4@gmail.com>
2016-01-30 21:59:55 -0500

    Agregando mayor soporte a resolucion de paths cuando el directorio de trabajo no es en la carpeta de los scripts

--- src/services/max_dev/root_path.py
+++ src/services/max_dev/root_path.py
@@ -1,5 +1,12 @@
 import os
 root_path = ''
 
+
+def set_root_path_from(main_file):
+    global root_path
+    root_path = os.path.dirname(os.path.realpath(main_file))
+
+
 def resolve_path(sub_path):
     return os.path.join(root_path, sub_path)
+
--- src/services/sap_media_server.py
+++ src/services/sap_media_server.py
@@ -1,7 +1,9 @@
 import os
 import sap_media_api
+from max_dev import root_path
 
 
 if __name__ == '__main__':
+    root_path.set_root_path_from(__file__)
     port = int(os.environ.get('PORT', 8080))
     sap_media_api.app.run(host='127.0.0.1', port=port)
